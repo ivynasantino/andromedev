c4b9c419f98a00aa3f6413b244d67361c8a1d486
Thayanne Sousa <thayannevls@gmail.com>
2020-07-21 21:41:18 -0300

    Add fontawesomeicons

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 const Item = ({ children, slug, className }) => (
   <Link
@@ -11,20 +13,25 @@ const Item = ({ children, slug, className }) => (
 );
 const Navbar = () => {
   return (
-    <aside className="w-64 my-2 absolute hidden md:block lg:block">
-      <nav className="flex flex-col">
-        <Item slug="/guia">Introdução</Item>
-        <Item slug="/guia/organizacao">Guia da Organização</Item>
-        <Item slug="/guia/organizacao/projeto" className="ml-4">
-          Criação de Projeto
-        </Item>
-        <Item slug="/guia/organizacao/projeto/repositorio" className="ml-8">
-          Requisitos do Repositório
-        </Item>
-        <Item slug="/guia/mentor">Guia do Mentor</Item>
-        <Item slug="/guia/aprendiz">Guia do Aprendiz</Item>
-      </nav>
-    </aside>
+    <>
+      <button className="bg-dark-gray right-0 bottom-0 rounded-full">
+        <FontAwesomeIcon icon={faCoffee} />
+      </button>
+      <aside className="w-64 my-2 absolute hidden md:block lg:block">
+        <nav className="flex flex-col">
+          <Item slug="/guia">Introdução</Item>
+          <Item slug="/guia/organizacao">Guia da Organização</Item>
+          <Item slug="/guia/organizacao/projeto" className="ml-4">
+            Criação de Projeto
+          </Item>
+          <Item slug="/guia/organizacao/projeto/repositorio" className="ml-8">
+            Requisitos do Repositório
+          </Item>
+          <Item slug="/guia/mentor">Guia do Mentor</Item>
+          <Item slug="/guia/aprendiz">Guia do Aprendiz</Item>
+        </nav>
+      </aside>
+    </>
   );
 };
 
